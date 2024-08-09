76b2e70f388ac1ab6f4dc7a87f194ff11791216c
shiva <shivajreddy@outlook.com>
2024-08-09 10:34:12 -0400

    .

--- cherno/classes/src/coin_change.cpp
+++ cherno/classes/src/coin_change.cpp
@@ -9,7 +9,7 @@ using std::cout, std::string, std::vector, std::unordered_map;
 int rec(int amount, vector<int> &coins, unordered_map<int, int> &cache);
 
 int Solution::coinChage(vector<int> &coins, int amount) {
-  cout << "Running solution" << "\n ";
+  cout << "Running solution" << "\n";
   unordered_map<int, int> cache;
   rec(amount, coins, cache);
   return -1;
--- cherno/classes/src/main.cpp
+++ cherno/classes/src/main.cpp
@@ -8,8 +8,16 @@ using std::vector;
 
 int main() {
   cout << "Problem: Coin Change \n";
+
   Solution solution;
 
-  vector<int> coins = vector<int>{1, 2, 5};
-  cout << solution.coinChage(coins, 11) << std::endl;
+  {
+    vector<int> coins = vector<int>{1, 2, 5};
+    cout << solution.coinChage(coins, 11) << std::endl;
+  }
+
+  {
+    vector<int> coins = vector<int>{2, 5};
+    cout << solution.coinChage(coins, 1) << std::endl;
+  }
 }

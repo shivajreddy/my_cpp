522d327a535669719b7b64300f5e166179aadf0e
shiva <smbp@shiva.local>
2024-08-10 12:26:04 -0400

    .

--- cherno/classes/src/main.cpp
+++ cherno/classes/src/main.cpp
@@ -10,21 +10,21 @@ int main() {
     std::vector<int> coins = {1, 2, 5};
     int amount = 11;
     int answer = 3;
-    std::cout << std::setw(3) << answer << "  |  "
+    std::cout << std::setw(3) << answer << " | "
               << sol.coinChange(coins, amount) << "\n";
   }
   {
     std::vector<int> coins = {1, 2, 5};
     int amount = 0;
     int answer = 0;
-    std::cout << std::setw(3) << answer << "  |  "
+    std::cout << std::setw(3) << answer << " | "
               << sol.coinChange(coins, amount) << "\n";
   }
   {
     std::vector<int> coins = {2, 5};
     int amount = 1;
     int answer = -1;
-    std::cout << std::setw(3) << answer << "  |  "
+    std::cout << std::setw(3) << answer << " | "
               << sol.coinChange(coins, amount) << "\n";
   }
 

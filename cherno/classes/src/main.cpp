7a783e0b689f748c742b500c3bfd2348a780f495
shiva <smbp@shiva.local>
2024-08-09 22:29:46 -0400

    .

--- cherno/classes/src/main.cpp
+++ cherno/classes/src/main.cpp
@@ -40,10 +40,24 @@ class Solution {
 
 int main() {
   Solution sol;
-  std::vector<int> coins = {1, 2, 5};
-  int amount = 11;
-  std::cout << "Minimum coins needed: " << sol.coinChange(coins, amount)
-            << std::endl;
+  {
+    std::vector<int> coins = {1, 2, 5};
+    int amount = 11;
+    int answer = 3;
+    std::cout << answer << " | " << sol.coinChange(coins, amount) << std::endl;
+  }
+  {
+    std::vector<int> coins = {1, 2, 5};
+    int amount = 0;
+    int answer = 0;
+    std::cout << answer << " | " << sol.coinChange(coins, amount) << std::endl;
+  }
+  {
+    std::vector<int> coins = {2, 5};
+    int amount = 1;
+    int answer = -1;
+    std::cout << answer << " | " << sol.coinChange(coins, amount) << std::endl;
+  }
 
   return 0;
 }

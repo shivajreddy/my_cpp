748ec34af9003b43483bd346e4db8d221564981d
shiva <shivajreddy@outlook.com>
2024-10-04 08:29:49 -0400

    .

---
+++ graphics/YoutubeOpenGL/main.cpp
@@ -0,0 +1,17 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// Entry Point for program
+int main() {
+  // Create Window
+
+  // Failed to create window
+  if (window == NULL) {
+    glfwTerminate();
+    return -1;
+  }
+
+  // Exit Program
+  glfwTerminate();
+  return 0;
+}

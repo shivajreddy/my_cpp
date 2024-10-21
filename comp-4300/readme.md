0f1da8c2e977b168162d17ca8c4280a9bc2bdace
shiva <shivajreddy@outlook.com>
2024-10-21 12:17:30 -0400

    new repo for COMP 4300

---
+++ comp-4300/readme.md
@@ -0,0 +1,3 @@
+
+[Youtube Playlist](https://www.youtube.com/playlist?list=PL_xRyXins84_Sq7yZkxGP_MgYAH-Zo8Uu)  
+
---
+++ comp-4300/video-01/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+  cout << "Video 01" << endl;
+  return 0;
+}

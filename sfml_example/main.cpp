a9c5f58c9ecf8bd5c85f1daae812847e7e0e9a46
shiva <shivajreddy@outlook.com>
2024-10-25 21:20:27 -0400

    .

---
+++ sfml_example/main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/VideoMode.hpp>
+
+int main() {
+  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML WINDOW");
+
+  sf::CircleShape circle(50);
+  circle.setFillColor(sf::Color::Green);
+
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear(sf::Color::Black);
+    window.draw(circle);
+    window.display();
+  }
+
+  return 0;
+}

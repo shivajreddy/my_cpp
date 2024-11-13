242dd256cb3e48762e1ae372aa6e050330096117
shiva <shivajreddy@outlook.com>
2024-11-13 08:01:17 -0500

    .

--- language/arrays/main.cpp
+++ language/arrays/main.cpp
@@ -2,9 +2,46 @@
 
 using namespace std;
 
-int x = 10;
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    // Constructor
+    ListNode(int val = -1, ListNode *next = nullptr) : val(val), next(next) {}
+};
+
+void print_linked_list(ListNode *root) {
+    while (root != nullptr) {
+        cout << root->val << " -> ";
+        root = root->next;
+    }
+    cout << "nullptr\n";
+}
+
+void delete_linked_list(ListNode *head) {
+    while (head != nullptr) {
+        ListNode *temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+ListNode *create_sample_linked_list() {
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    return head;
+}
 
 int main() {
-    cout << "hi";
+
+    ListNode *root = create_sample_linked_list();
+
+    print_linked_list(root);
+
+    delete_linked_list(root);
+
     return 0;
 }

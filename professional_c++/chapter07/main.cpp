3a81ac863962391ce970ea8dbe6e604e32b76515
shiva <shivajreddy@outlook.com>
2024-11-22 23:47:40 -0500

    .

--- professional_c++/chapter07/main.cpp
+++ professional_c++/chapter07/main.cpp
@@ -3,9 +3,15 @@ import <print>;
 
 using namespace std;
 
-int main(void)
+void allocate_and_deallocate()
 {
-    println(" ---------------- Memory Management ---------------- ");
+
+    /* KEY WAYS TO LOOK AT IT
+            new <type>;
+            new <type>[];
+
+            <type> can be int, Person, or Person*
+    */
 
     {
         // --------------------------------------------
@@ -13,6 +19,9 @@ int main(void)
         // --------------------------------------------
         int *ptr {new int}; // Allocated the size_of(int) on heap, and stores
                             // its address on heap
+
+        // Right now *ptr is some random value
+        *ptr = 10;
 
         // Deallocate Memory & Set pointer to nullptr
         delete ptr;
@@ -50,8 +59,54 @@ int main(void)
     }
 
     {
-        // Dyna
+        // --------------------------------------------
+        // Dynamically allocated array after use deallocate
+        // set ptr to nullptr
+        // --------------------------------------------
+        size_t size = 5; // can also be of type int
+        int *arr {new int[size]};
+
+        delete[] arr;
+        arr = nullptr;
     }
+}
+
+// Array of objects
+class Simple
+{
+public:
+    Simple() { println("Simple constructor called!"); }
+    ~Simple() { println("Simple destructor called!"); }
+};
+
+void multiple_allocations()
+{
+    const size_t size {5};
+    Simple **mySimplePtrArray {new Simple *[size]};
+
+    // Allocate an object for each pointer
+    for (size_t i {0}; i < size; ++i) {
+        mySimplePtrArray[i] = new Simple {};
+    }
+
+    // use mySimplePtrArray
+
+    // Delete each allocated object
+    for (size_t i {0}; i < size; ++i) {
+        delete mySimplePtrArray[i];
+        mySimplePtrArray[i] = nullptr;
+    }
+
+    // Delete the array itself
+    delete[] mySimplePtrArray;
+    mySimplePtrArray = nullptr;
+}
+
+int main(void)
+{
+    println("---------------- Memory Management ----------------");
+    // allocate_and_deallocate();
+    multiple_allocations();
 
     return 0;
 }

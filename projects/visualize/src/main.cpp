94b44c45988b6745fc408f9410d1dc5319eee1c0
shiva <shivajreddy@outlook.com>
2024-12-16 16:10:11 -0500

    [BACKEND] cleaned up main

--- projects/visualize/src/main.cpp
+++ projects/visualize/src/main.cpp
@@ -14,8 +14,7 @@ int main()
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20,
-                 LIGHTGRAY);
+        DrawText("Array visual!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
     }

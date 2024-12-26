ca9f135eab9094d4659f574417ab2adb7842a413
smpl <shivajreddy@outlook.com>
2024-12-26 07:46:00 -0500

    .

--- projects/networking/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello There" << endl;
-    return 0;
-}
---
+++ projects/networking/src/main.cpp
@@ -0,0 +1,31 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello There" << endl;
+    return 0;
+}
+*/
+
+#include <asio.hpp>
+#include <iostream>
+
+int main()
+{
+    try {
+        asio::io_context io_context;
+
+        asio::steady_timer timer(io_context, asio::chrono::seconds(3));
+        timer.wait();
+
+        std::cout << "Hello, ASIO! Timer expired after 3 seconds." << std::endl;
+    }
+    catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
